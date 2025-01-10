dde99b294ff03c22824534e74ae0848b51fe3b80
danie <dani89rosa@gmail.com>
2025-01-10 10:48:57 +0100

    ejercicio 4 y 5

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
 
 //        VariablesCondiciones.coeficientes(a,b,c);//1. Ecuación de segundo grado
 //        VariablesCondiciones.areaCirculo(r);// 2. Calcular área círculo.
-        System.out.println("La longitud es: "+VariablesCondiciones.longitudCirculo(r)); // 3. Calcular longitud círculo.
-
-
+//        System.out.println("La longitud es: "+VariablesCondiciones.longitudCirculo(r)); // 3. Calcular longitud círculo.
+//        System.out.println(VariablesCondiciones.numerosIguales(a,b));// 4. Números iguales o no.
+        System.out.println(VariablesCondiciones.numerosPositivos(a)); // 5. positivo o negativo
 
     }
 
--- src/VariablesCondiciones.java
+++ src/VariablesCondiciones.java
@@ -4,7 +4,7 @@ public class VariablesCondiciones {
     public static Scanner entradaEscaner = new Scanner(System.in);
 
     //1. Pedir los coeficientes de una ecuación se 2º grado, y muestre sus soluciones reales. Si no existen, debe indicarlo.
-    public static void coeficientes(double a,double b,double c) {
+    public static void coeficientes(double a, double b, double c) {
 
 //        double a, b, c;
         double x1, x2, d;
@@ -40,14 +40,36 @@ public class VariablesCondiciones {
         System.out.println("El área del circulo con radio " + r + " es: " + a);
     }
 
-//    3. Pedir el radio de una circunferencia y calcular su longitud.
-    public static double longitudCirculo(double r){
+    //    3. Pedir el radio de una circunferencia y calcular su longitud.
+    public static double longitudCirculo(double r) {
         double l;
         System.out.print("Introduce el radio círculo: ");
         r = entradaEscaner.nextDouble();
-        l = 2*Math.PI*r;
+        l = 2 * Math.PI * r;
 
         return l;
 
+    }
+
+    //    4. Pedir dos números y decir si son iguales o no.
+    public static String numerosIguales(double a, double b) {
+        System.out.print("Introduce número uno: ");
+        a = entradaEscaner.nextDouble();
+        System.out.print("Introduce número dos: ");
+        b = entradaEscaner.nextDouble();
+
+        if (a == b) return "Los números son iguales.";
+        else if (a > b) return "El " + a + " es más grande que " + b;
+        else return "El " + b + " es más grande que " + a;
+
+    }
+
+    //    5. Pedir un número e indicar si es positivo o negativo.
+    public static String numerosPositivos(double a) {
+        System.out.print("Introduce número: ");
+        a = entradaEscaner.nextDouble();
+
+        if (a >= 0) return "Es positivo";
+        else return "Es negativo";
     }
 }
